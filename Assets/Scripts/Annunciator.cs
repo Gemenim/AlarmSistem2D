585868fb8c4a289924a1cbd7d743ab2da33c627c
Gemenim <121296811+Gemenim@users.noreply.github.com>
2023-11-16 18:48:08 +0300

    AddCorutines

--- Assets/Scripts/Annunciator.cs
+++ Assets/Scripts/Annunciator.cs
@@ -11,6 +11,7 @@ public class Annunciator : MonoBehaviour
     private AudioSource _audioSource;
     private IntruderLocator _intruderLocator;
     private float _maxVolume = 1.0f;
+    private bool _isCorutinJob = false;
 
     private void Start()
     {
@@ -21,9 +22,28 @@ public class Annunciator : MonoBehaviour
 
     private void Update()
     {
-        if (_intruderLocator.IsIntruder)
-            _audioSource.volume += _maxVolume * Time.deltaTime * _rate—hange;
-        else
-            _audioSource.volume -= _maxVolume * _rate—hange * Time.deltaTime;
+        if (_intruderLocator.IsIntruder && _isCorutinJob == false)
+        {
+            _isCorutinJob = true;
+            StartCoroutine(ChangeVolume());
+        }
+    }
+
+    private IEnumerator ChangeVolume()
+    {
+        while (_isCorutinJob)
+        {
+            if (_intruderLocator.IsIntruder)
+                _audioSource.volume += _maxVolume * Time.deltaTime * _rate—hange;
+            else
+            {
+                _audioSource.volume -= _maxVolume * _rate—hange * Time.deltaTime;
+
+                if (_audioSource.volume == 0)
+                    _isCorutinJob = false;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
